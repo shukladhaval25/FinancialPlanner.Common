3bbd3f5edc0853d426e67a1d516f2b6b3d64a54f
shukladhaval25 <shukla_dhaval@yahoo.com>
2019-11-14 00:21:35 +0530

    Add model for change of name and minor to major transaction.

---
+++ Model/TaskManagement/MFTransactions/ChangeOfName.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialPlanner.Common.Model.TaskManagement.MFTransactions
+{
+    public class ChangeOfName : Base
+    {
+        int id;
+        int taskId;
+        int arn;
+        int cid;
+        string clientGroup;
+        string fromMemberName;
+        string toMemberName;
+        int amc;
+        string folioNumber;
+        bool isSignatureChanged;
+        string modeOfExecution;
+
+        public int Id { get => id; set => id = value; }
+        public int TaskId { get => taskId; set => taskId = value; }
+        public int Arn { get => arn; set => arn = value; }
+        public int Cid { get => cid; set => cid = value; }
+        public string ClientGroup { get => clientGroup; set => clientGroup = value; }
+        public string FromMemberName { get => fromMemberName; set => fromMemberName = value; }
+        public int Amc { get => amc; set => amc = value; }
+        public string FolioNumber { get => folioNumber; set => folioNumber = value; }
+        public string ModeOfExecution { get => modeOfExecution; set => modeOfExecution = value; }
+        public string ToMemberName { get => toMemberName; set => toMemberName = value; }
+        public bool IsSignatureChanged { get => isSignatureChanged; set => isSignatureChanged = value; }
+    }
+}
---
+++ Model/TaskManagement/MFTransactions/MinorToMajor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialPlanner.Common.Model.TaskManagement.MFTransactions
+{
+    public class MinorToMajor : Base
+    {
+        int id;
+        int taskId;
+        int arn;
+        int cid;
+        string clientGroup;
+        string minorName;
+        string guardian;
+        int amc;
+        string folioNumber;
+        string modeOfExecution;
+
+        public int Id { get => id; set => id = value; }
+        public int TaskId { get => taskId; set => taskId = value; }
+        public int Arn { get => arn; set => arn = value; }
+        public int Cid { get => cid; set => cid = value; }
+        public string ClientGroup { get => clientGroup; set => clientGroup = value; }
+        public string MinorName { get => minorName; set => minorName = value; }
+        public int Amc { get => amc; set => amc = value; }
+        public string FolioNumber { get => folioNumber; set => folioNumber = value; }
+        public string ModeOfExecution { get => modeOfExecution; set => modeOfExecution = value; }
+        public string Guardian { get => guardian; set => guardian = value; }
+    }
+}
