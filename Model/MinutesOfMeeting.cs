11332f003167de292b95911efd6bc87430502aba
shukladhaval25 <shukla_dhaval@yahoo.com>
2020-02-17 23:47:12 +0530

    Model of mom

---
+++ Model/MinutesOfMeeting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialPlanner.Common.Model
+{
+    public class MinutesOfMeeting
+    {
+        public int Id { get; set; }
+        public DateTime MeetingDate { get; set; }
+        public int ClientId { get; set; }
+        public string Person { get; set; }
+        public string MeetingType { get; set; }
+        public List<MinutesOfMeetingDetails> MinutesOfMeetingDetails { get; set; }
+    }
+}
---
+++ Model/MinutesOfMeetingDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialPlanner.Common.Model
+{
+    public class MinutesOfMeetingDetails
+    {
+        public int MOMID { get; set; }              
+        public string Points { get; set; }
+        public string ActionPlan { get; set; }
+        public string Responsibility { get; set; }
+        public string Status { get; set; }
+        public bool IsTaskCreationRequire { get; set; }
+        public int? TaskId { get; set; }
+    }
+}
