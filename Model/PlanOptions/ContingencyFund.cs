50ef796bee4e51b8f6f6805b44fe4bf0dca39ad7
shukla <shukl@DESKTOP-FT786DU>
2021-03-09 17:18:42 +0530

    Add contingency fund model

---
+++ Model/PlanOptions/ContingencyFund.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialPlanner.Common.Model.PlanOptions
+{
+    public class ContingencyFund : Base 
+    {
+        int _id;
+        int _plannerId;
+        int _optionId;
+        double _amount;
+
+        public int Id { get => _id; set => _id = value; }
+        public int PlannerId { get => _plannerId; set => _plannerId = value; }
+        public int OptionId { get => _optionId; set => _optionId = value; }
+        public double Amount { get => _amount; set => _amount = value; }
+    }
+}
