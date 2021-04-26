cd51056884d100157c2326634956fe23e6be15bc
shukla <shukl@DESKTOP-FT786DU>
2021-04-26 16:25:02 +0530

    add networth model

---
+++ Model/Planner/NetWorth.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialPlanner.Common.Model
+{
+    public class NetWorth : Base 
+    {
+        public int Id { get; set; }
+        public int CId { get; set; }
+        public int Year { get; set; }
+        public double Amount { get; set; }
+    }
+}
