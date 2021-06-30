59f34aa96d05a19e6e64914106f7555d253cb679
shukla <shukl@DESKTOP-FT786DU>
2021-06-30 11:27:59 +0530

    Add model for score

---
+++ Model/ScoreCalculation/Score.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialPlanner.Common.Model.ScoreCalculation
+{
+    public class Score
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public float MaxValue { get; set; }
+        public float MinValue { get; set; }
+        public float Weightage { get; set; }
+    }
+}
---
+++ Model/ScoreCalculation/ScoreEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialPlanner.Common.Model.ScoreCalculation
+{
+    public class ScoreEntry
+    {
+        public DateTime EntryDate { get; set; }
+        public string Title { get; set; }
+        public float Value { get; set; }
+    }
+}
